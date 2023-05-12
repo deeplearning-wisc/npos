d66dc88f89f44a09ff8dbc921c76e9b81beb21f4
Xuefeng Du <42554178+d12306@users.noreply.github.com>
2023-05-12 16:24:23 -0500

    Update test_npos_imagenet100.sh

--- training_from_scratch/test_npos_imagenet100.sh
+++ training_from_scratch/test_npos_imagenet100.sh
@@ -1,1 +1,1 @@
-python eval_ood_detection.py --in_dataset ImageNet-100 --model resnet101 --ckpt ImageNet-100_scratch.pt --T 300
+python eval_ood_detection_in100.py --in_dataset ImageNet-100 --model resnet101 --ckpt ImageNet-100_scratch.pt --T 300 --feat_dim 512
