3e252258f027caea0521a8bd6c0df60eaaba2954
Xuefeng Du <42554178+d12306@users.noreply.github.com>
2023-05-12 16:52:54 -0500

    Update eval_utils.py

--- training_from_scratch/evaluation/eval_utils.py
+++ training_from_scratch/evaluation/eval_utils.py
@@ -58,31 +58,66 @@ import torchvision.transforms as trn
 normalize = trn.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 def set_loader_in100(args):
-    train_dataset = \
-        torchvision.datasets.ImageFolder(
-            os.path.join('/nobackup-slow/dataset/my_xfdu/IN100_new/', 'train'),
-            trn.Compose([
-                trn.Resize(256),
-                trn.CenterCrop(224),
-                trn.ToTensor(),
-                normalize,
-            ]))
-    val_dataset = \
-        torchvision.datasets.ImageFolder(
-            os.path.join('/nobackup-slow/dataset/my_xfdu/IN100_new/', 'val'),
-            trn.Compose([
-                trn.Resize(256),
-                trn.CenterCrop(224),
-                trn.ToTensor(),
-                normalize,
-            ]))
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batch_size, shuffle=False,
-        num_workers=8, pin_memory=True)
-    val_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=args.batch_size, shuffle=False,
-        num_workers=8, pin_memory=True)
-    return train_loader, val_loader
+    import torchvision.datasets as dset
+    from image_folder import ImageSubfolder
+    test_transform = trn.Compose([
+        trn.Resize(size=(224, 224), interpolation=trn.InterpolationMode.BICUBIC),
+        trn.CenterCrop(size=(224, 224)),
+        trn.ToTensor(),
+        trn.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711))
+    ])
+    root_dir = '/nobackup-slow/dataset/ILSVRC-2012/'
+    train_dir = root_dir + 'val'
+    classes, _ = dset.folder.find_classes(train_dir)
+    index = [125, 788, 630, 535, 474, 694, 146, 914, 447, 208, 182, 621, 271, 646, 328, 119, 772, 928, 610, 891, 340,
+             890, 589, 524, 172, 453, 869, 556, 168, 982, 942, 874, 787, 320, 457, 127, 814, 358, 604, 634, 898, 388,
+             618, 306, 150, 508, 702, 323, 822, 63, 445, 927, 266, 298, 255, 44, 207, 151, 666, 868, 992, 843, 436, 131,
+             384, 908, 278, 169, 294, 428, 60, 472, 778, 304, 76, 289, 199, 152, 584, 510, 825, 236, 395, 762, 917, 573,
+             949, 696, 977, 401, 583, 10, 562, 738, 416, 637, 973, 359, 52, 708]
+    # index = [6, 8, 18, 21, 23, 42, 78, 85, 94, 105, 107, 113, 124, 130, 148, 151, 272, 281, 288, 291, 292, 301, 308,
+    #          310, 311, 323, 327, 331, 333, 337, 340, 341, 345, 352, 357, 360, 366, 388, 394, 401, 403, 407, 411, 414,
+    #          417, 420, 425, 429, 430, 437, 446, 462, 470, 483, 488, 492, 497, 498, 516, 525, 526, 538, 541, 549, 561,
+    #          562, 578, 587, 608, 609, 616, 620, 621, 624, 629, 643, 646, 650, 651, 657, 677, 711, 721, 742, 748, 764,
+    #          774, 783, 819, 827, 843, 851, 866, 872, 879, 889, 902, 913, 937, 949]
+
+    num_classes = 100
+    classes = [classes[i] for i in index]
+    class_to_idx = {c: i for i, c in enumerate(classes)}
+    train_data = ImageSubfolder(root_dir + 'train', transform=test_transform, class_to_idx=class_to_idx)
+    test_data = ImageSubfolder(root_dir + 'val', transform=test_transform, class_to_idx=class_to_idx)
+    labeled_trainloader = torch.utils.data.DataLoader(train_data, \
+                                                      batch_size=args.batch_size, shuffle=True, num_workers=16,
+                                                      pin_memory=True, drop_last=True)
+    testloader = torch.utils.data.DataLoader(test_data,
+                                             batch_size=args.batch_size, shuffle=True, num_workers=16, pin_memory=True)
+
+
+
+    # train_dataset = \
+    #     torchvision.datasets.ImageFolder(
+    #         os.path.join('/nobackup-slow/dataset/my_xfdu/IN100_new/', 'train'),
+    #         trn.Compose([
+    #             trn.Resize(256),
+    #             trn.CenterCrop(224),
+    #             trn.ToTensor(),
+    #             normalize,
+    #         ]))
+    # val_dataset = \
+    #     torchvision.datasets.ImageFolder(
+    #         os.path.join('/nobackup-slow/dataset/my_xfdu/IN100_new/', 'val'),
+    #         trn.Compose([
+    #             trn.Resize(256),
+    #             trn.CenterCrop(224),
+    #             trn.ToTensor(),
+    #             normalize,
+    #         ]))
+    # train_loader = torch.utils.data.DataLoader(
+    #     train_dataset, batch_size=args.batch_size, shuffle=False,
+    #     num_workers=8, pin_memory=True)
+    # val_loader = torch.utils.data.DataLoader(
+    #     val_dataset, batch_size=args.batch_size, shuffle=False,
+    #     num_workers=8, pin_memory=True)
+    return labeled_trainloader, testloader
 
 
 def set_ood_loader(args, out_dataset):
@@ -181,7 +216,7 @@ def obtain_feature_from_loader(net, loader, layer_idx, embedding_dim, num_batche
                 if batch_idx >= num_batches:
                     break
             data, target = data.cuda(), target.cuda()
-            out_feature = net.intermediate_forward(data, layer_idx)
+            out_feature = net.intermediate_forward(data, layer_idx) 
             if layer_idx == 0: # out_feature: bz, 512, 4, 4
                 out_feature = out_feature.view(out_feature.size(0), out_feature.size(1), -1) #bz, 512, 16
                 out_feature = torch.mean(out_feature, 2) # bz, 512
