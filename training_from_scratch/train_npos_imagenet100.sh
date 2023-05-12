8b0cf85d0cbb4c60969e9e07800515661a843dab
Xuefeng Du <42554178+d12306@users.noreply.github.com>
2023-05-11 22:21:43 -0500

    Update train_npos_imagenet100.sh

--- training_from_scratch/train_npos_imagenet100.sh
+++ training_from_scratch/train_npos_imagenet100.sh
@@ -1,1 +1,1 @@
-python train_imagenet.py --in-dataset ImageNet-100 --model resnet101 --temp 0.1 --sample_from 1000 --K 400 --learning_rate 0.1 --batch-size 512 --cov_mat 0.1 --start-epoch 200 --ID_points_num 300
+python train_imagenet.py --in-dataset ImageNet-100 --model resnet101 --temp 0.1 --sample_from 1000 --K 400 --learning_rate 0.1 --batch-size 512 --cov_mat 0.1 --start_epoch_KNN 200 --ID_points_num 300
